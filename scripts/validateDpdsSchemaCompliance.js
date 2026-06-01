6f83dd4d8bb93998492372473edc4b2fc5f13d73
James Reed <james.reed1@justice.gov.uk>
2026-06-01 14:19:44 +0100

    use id to ref a url instead

--- scripts/validateDpdsSchemaCompliance.js
+++ scripts/validateDpdsSchemaCompliance.js
@@ -9,9 +9,9 @@ const main = async () => {
     const ajv = new Ajv({ allErrors: true, strict: true })
     // Adding these schemas automatically validates them as per https://ajv.js.org/api.html#ajv-validateschema-schema-object-boolean
     const validate = ajv
-        .addSchema(JSON.parse(fs.readFileSync("schema/input-ports-schema.json")))
-        .addSchema(JSON.parse(fs.readFileSync("schema/output-ports-schema.json")))
-        .addSchema(JSON.parse(fs.readFileSync("schema/policy-schema.json")))
+        // .addSchema(JSON.parse(fs.readFileSync("schema/input-ports-schema.json")))
+        // .addSchema(JSON.parse(fs.readFileSync("schema/output-ports-schema.json")))
+        // .addSchema(JSON.parse(fs.readFileSync("schema/policy-schema.json")))
         .compile(JSON.parse(fs.readFileSync("schema/data-product-definition-schema.json")))
         
     let valid = true;
