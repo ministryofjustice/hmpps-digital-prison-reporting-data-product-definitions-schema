45a71c0f23d87b8582a7bfa4b0546495954b1683
James Reed <james.reed1@justice.gov.uk>
2026-06-02 16:03:51 +0100

    versioned folders

--- scripts/validateDpdsSchemaCompliance.js
+++ scripts/validateDpdsSchemaCompliance.js
@@ -9,11 +9,10 @@ const main = async () => {
     const ajv = new Ajv({ allErrors: true, strict: true, loadSchema: async (uri) => { return (await (await fetch(uri)).json()) } })
     // Adding these schemas automatically validates them as per https://ajv.js.org/api.html#ajv-validateschema-schema-object-boolean
     const validate = await ajv
-        .compileAsync(JSON.parse(fs.readFileSync("schema/data-product-definition-schema.json"))).then((validate) => {
-
+        .compileAsync(JSON.parse(fs.readFileSync("schema/2.0.0/data-product-definition-schema.json"))).then((validate) => {
             let valid = true;
-            
-            const data = JSON.parse(fs.readFileSync("schema/test-dpd.json"));
+            const data = JSON.parse(fs.readFileSync("scripts/test-dpd.json"));
+
             if (!validate(data)) {
                 const filteredErrors = validate.errors
                 if (filteredErrors.length > 0) {
@@ -21,10 +20,8 @@ const main = async () => {
                     valid = false;
                 }
             }
-        
             if (!valid) process.exit(1);
         })
-        
 }
 
 main()
