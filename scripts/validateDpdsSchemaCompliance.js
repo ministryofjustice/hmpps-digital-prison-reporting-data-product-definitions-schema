6f5e72c1c014d916e86d7039de9963c6708cc40d
James Reed <james.reed1@justice.gov.uk>
2026-06-02 10:05:36 +0100

    use main ref, add couple scripts to gha

--- scripts/validateDpdsSchemaCompliance.js
+++ scripts/validateDpdsSchemaCompliance.js
@@ -6,26 +6,25 @@ const Ajv = require("ajv");
 const fs = require("fs");
 
 const main = async () => {
-    const ajv = new Ajv({ allErrors: true, strict: true })
+    const ajv = new Ajv({ allErrors: true, strict: true, loadSchema: async (uri) => { return (await (await fetch(uri)).json()) } })
     // Adding these schemas automatically validates them as per https://ajv.js.org/api.html#ajv-validateschema-schema-object-boolean
-    const validate = ajv
-        // .addSchema(JSON.parse(fs.readFileSync("schema/input-ports-schema.json")))
-        // .addSchema(JSON.parse(fs.readFileSync("schema/output-ports-schema.json")))
-        // .addSchema(JSON.parse(fs.readFileSync("schema/policy-schema.json")))
-        .compile(JSON.parse(fs.readFileSync("schema/data-product-definition-schema.json")))
-        
-    let valid = true;
-    
-    const data = JSON.parse(fs.readFileSync("schema/test-dpd.json"));
-    if (!validate(data)) {
-        const filteredErrors = validate.errors
-        if (filteredErrors.length > 0) {
-            console.error(JSON.stringify(filteredErrors, null, 2));
-            valid = false;
-        }
-    }
+    const validate = await ajv
+        .compileAsync(JSON.parse(fs.readFileSync("schema/data-product-definition-schema.json"))).then((validate) => {
 
-    if (!valid) process.exit(1);
+            let valid = true;
+            
+            const data = JSON.parse(fs.readFileSync("schema/test-dpd.json"));
+            if (!validate(data)) {
+                const filteredErrors = validate.errors
+                if (filteredErrors.length > 0) {
+                    console.error(JSON.stringify(filteredErrors, null, 2));
+                    valid = false;
+                }
+            }
+        
+            if (!valid) process.exit(1);
+        })
+        
 }
 
 main()
